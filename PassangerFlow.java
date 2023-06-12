b8b79c13734f251f6e8a7d0e9db9aae99d93de06
河口 欣仁 <kawakin0310@icloud.com>
2023-06-12 12:06:58 +0900

    🎉乗客の流れを表すクラスを作成

---
+++ PassangerFlow.java
@@ -0,0 +1,36 @@
+class PassangerFlow {
+    private Station station;
+    private int passengersIn;
+    private int passengersOut;
+
+    public PassangerFlow(Station station, int passengersIn, int passengersOut) {
+        this.station = station;
+        this.passengersIn = passengersIn;
+        this.passengersOut = passengersOut;
+    }
+
+    public Station getStation() {
+        return this.station;
+    }
+
+    public void setStation(Station station) {
+        this.station = station;
+    }
+
+    public int getPassengersIn() {
+        return this.passengersIn;
+    }
+
+    public void setPassengersIn(int passengersIn) {
+        this.passengersIn = passengersIn;
+    }
+
+    public int getPassengersOut() {
+        return this.passengersOut;
+    }
+
+    public void setPassengersOut(int passengersOut) {
+        this.passengersOut = passengersOut;
+    }
+
+}
